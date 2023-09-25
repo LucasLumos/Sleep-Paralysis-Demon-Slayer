82e8779bf226d4d9aa9de2b680fa7022a5cc017b
LucasLumos <lucas@lumoslux.com>
2023-09-25 07:54:03 -0700

    Initial Commit

---
+++ Sleep Paralysis Demon Slayer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Sleep Paralysis Demon Slayer
+//
+//  Created by Lucas Tang on 9/25/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
